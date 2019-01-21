099aa83b8c54ec6d3143393491fa81f92f7925c7
AnthonyBuncio <anthonyb.ft@gmail.com>
2019-01-20 22:45:53 -0600

    profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const validateProfileInput = require('../../validation/profile');
+
 // Import profile model
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -12,6 +14,7 @@ router.get('/test', (req, res) => res.json({msg: 'Profile work'}));
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const errors = {};
     Profile.findOne({user: req.user.id})
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             if (!profile) {
                 errors.noprofile = 'There is no profile for this user';
@@ -23,6 +26,12 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 });
 
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const {errors, isValid} = validateProfileInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
