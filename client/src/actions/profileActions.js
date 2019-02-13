a7044102ef109bf5b9d0d15f6349a5a6e6cc3723
AnthonyBuncio <anthonyb.ft@gmail.com>
2019-02-13 17:26:51 -0600

    add experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -26,8 +26,18 @@ export const createProfile = (profileData, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })  
-    )
+      }))
+}
+
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios.post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
 }
 
 // Delete account & profile
