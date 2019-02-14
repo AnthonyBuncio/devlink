179ceed6e7c09565fcf068c4ee80ca927cbd843a
AnthonyBuncio <anthonyb.ft@gmail.com>
2019-02-14 00:48:36 -0600

    add and remove likes from posts

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import {ADD_POST, GET_POSTS, POST_LOADING} from '../actions/types';
+import {ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -8,16 +8,21 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch(action.type) {
-    case ADD_POST:
-      return {
-        ...state,
-        posts: [action.payload, ...state.posts]
-      }
     case GET_POSTS:
       return {
         ...state,
         posts: action.payload,
         loading: false
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      }
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
       }
     case POST_LOADING:
       return {
